7cf37c95abcc9f279d758e7690abc7be1723f345
watsonkalamari <klwatson3@ualr.edu>
2020-10-23 21:50:31 -0500

    Work in progress
    Most of the look and feel.

--- android-layouts-alt-master/app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ android-layouts-alt-master/app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -8,10 +8,16 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO 02. Create a new method called "calculateTotal" for calculating the invoice's total amount of money
 
+    public double calculateTotal(){
+
+    return 0.00;
+    }
     // TODO 03. Bind the "calculateTotal" method to the button with the "CALCULATE TOTAL" label
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
+        
     }
 }
