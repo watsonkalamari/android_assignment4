14b81bfae891dcf035bdd4bc248a1a1071e26576
watsonkalamari <klwatson3@ualr.edu>
2020-10-24 06:59:23 -0500

    Work in Progress

--- android-layouts-alt-master/app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ android-layouts-alt-master/app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -3,6 +3,9 @@ package com.ualr.layoutassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.ualr.layoutassignment.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,14 +13,31 @@ public class MainActivity extends AppCompatActivity {
 
     public double calculateTotal(){
 
-    return 0.00;
+        double sum;
+        sum=0.00;
+    return sum;
     }
     // TODO 03. Bind the "calculateTotal" method to the button with the "CALCULATE TOTAL" label
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        final ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
-        
+
+        binding.calculateBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                calculateTotal();
+            }
+        });
+
+
+
+
+
+
+
     }
 }
