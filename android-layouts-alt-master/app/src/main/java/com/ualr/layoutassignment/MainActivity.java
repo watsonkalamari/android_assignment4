14cc61c2491aa536cbde9c5744e0c5181c4b308b
watsonkalamari <klwatson3@ualr.edu>
2020-10-25 15:03:49 -0500

    Work in Progress
    Trying to get the total amount textview to change.

--- android-layouts-alt-master/app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ android-layouts-alt-master/app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 
+import com.google.android.material.checkbox.MaterialCheckBox;
 import com.ualr.layoutassignment.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO 02. Create a new method called "calculateTotal" for calculating the invoice's total amount of money
 
-    public double calculateTotal(){
+    public String calculateTotal(double prod1){
 
+        String returnedSum;
         double sum;
-        sum=0.00;
-    return sum;
+        sum=+prod1;
+       returnedSum= String.valueOf(sum);
+    return returnedSum;
     }
     // TODO 03. Bind the "calculateTotal" method to the button with the "CALCULATE TOTAL" label
 
@@ -26,10 +30,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
 
+
+
+
+
         binding.calculateBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                calculateTotal();
+                double prod1=0.0;
+
+                if(binding.product1Cbx.isChecked()==true)
+                {
+                    prod1= Double.valueOf(binding.product1Edtxt.getText().toString());
+
+                }
+             binding.totalAmountVw.setText(calculateTotal(prod1));
+
             }
         });
 
